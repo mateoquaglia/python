4bbd73efee93ab5d3913f3c4dc8727a6c7fc7457
mateoquaglia <mateo.quaglia@mi.unc.edu.ar>
2024-07-26 19:47:45 -0300

    generacion de contraseñas aleatorias

--- pythonPass.py
+++ pythonPass.py
@@ -1,6 +1,15 @@
 import string 
 import random 
 
-tamaño =  int(input("Introduce el tamaño de la contraseña: "))
+tamano =  int(input("Introduce el tamano de la contraseña: "))
 caracteres = string.ascii_letters + string.digits + string.punctuation
+
+contrasena = ("".join(random.choice(caracteres)for i in range(tamano))) 
+
+if input("deseas ver tu contrasena?") == "si":
+	print("tu contrasena es: " + contrasena)
+else:
+	input("deseas ver tu contrasena?") == "no"
+	print("esta bien")
+
 
