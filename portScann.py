d05506ecf74c4d39da0267e91adc66890f345393
mateoquaglia <mateo.quaglia@mi.unc.edu.ar>
2024-07-27 00:45:19 -0300

    better performance and range control

--- portScann.py
+++ portScann.py
@@ -1,16 +1,24 @@
 import socket
 
-ip = input("ingrese la ip a escanear: ")
-
-for puerto in range(1, 65535):
-
+def escanear_puerto(ip, puerto):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(1)
-
-    resultado = sock.connect_ex((ip, puerto))
-
-    if resultado == 0:
-        print("Puerto abierto: " + str(puerto))
+    try:
+        resultado = sock.connect_ex((ip, puerto))
+        if resultado == 0:
+            print(f"Puerto abierto: {puerto}")
+    except socket.error:
+        print(f"Error al conectar al puerto: {puerto}")
+    finally:
         sock.close()
-    else:
-        print("Puerto cerrado: " + str(puerto))
+
+def main():
+    ip = input("Ingrese la IP a escanear: ")
+    puerto_inicio = int(input("Ingrese el puerto de inicio: "))
+    puerto_fin = int(input("Ingrese el puerto final: "))
+
+    for puerto in range(puerto_inicio, puerto_fin + 1):
+        escanear_puerto(ip, puerto)
+
+if __name__ == "__main__":
+    main()
